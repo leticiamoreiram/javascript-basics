f603971ba155a96778deb4f2427932c025b7d97e
leticiamoreiram <leticia.m@gec.inatel.br>
2023-12-12 20:25:55 -0300

    grocery list

---
+++ Criando lista de compras/script.js
@@ -0,0 +1,15 @@
+/*
+
+    capture 10 itens para compor a lista de supermercado
+
+    depois, imprima-os separando por vírgula
+
+*/
+
+let items = [];
+for( let item = 0; item < 10; item++ ) {
+    itemName = prompt("Digite o item " + (item + 1));
+    items.push(itemName)
+}
+
+alert(items)
